558fa0b951d8b456d0e688c18966e8c6a33bb0e8
Nguyễn Dương Ân <nguyenduongan64@gmail.com>
2026-02-23 16:47:23 +0700

    add systemConfig controller

---
+++ src/controllers/systemConfig.controller.js
@@ -0,0 +1,22 @@
+import { responseSuccess } from '../common/helpers/function.helper.js';
+import { systemConfigService } from '../services/systemConfig.service.js';
+
+export const systemConfigController = {
+  async getIncidentThresholds(req, res) {
+    const data = await systemConfigService.getIncidentThresholds();
+    const response = responseSuccess(data, 'Configuration retrieved successfully');
+    res.status(response.statusCode).json(response);
+  },
+
+  async updateIncidentThresholds(req, res) {
+    const data = await systemConfigService.updateIncidentThresholds(req.body, req.user.id);
+    const response = responseSuccess(data, 'Configuration updated successfully');
+    res.status(response.statusCode).json(response);
+  },
+
+  async resetIncidentThresholds(req, res) {
+    const data = await systemConfigService.resetIncidentThresholds(req.user.id);
+    const response = responseSuccess(data, 'Configuration reset to defaults successfully');
+    res.status(response.statusCode).json(response);
+  },
+};
